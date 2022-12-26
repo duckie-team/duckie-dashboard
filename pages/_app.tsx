f56144ce498cab57365c9235a2734568ad1019d1
혀느현스 <hyuns@hyuns.dev>
2022-12-26 09:58:27 +0900

    feat: opize-design-system 및 라이브러리 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { OpizeWrapper } from "opize-design-system";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Flip, ToastContainer } from "react-toastify";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import "opize-design-system/dist/style/font.css";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import Head from "next/head";
+
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>Duckie Dashboard</title>
+                <meta name="description" content="Duckie Dashboard" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <OpizeWrapper>
+                <QueryClientProvider client={queryClient}>
+                    <Component {...pageProps} />
+                    <ReactQueryDevtools initialIsOpen={true} />
+                    <ToastContainer
+                        position="bottom-right"
+                        autoClose={3000}
+                        hideProgressBar={false}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        draggable
+                        transition={Flip}
+                    />
+                </QueryClientProvider>
+            </OpizeWrapper>
+        </>
+    );
 }
+
+export default MyApp;
